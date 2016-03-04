6bc7d2d97287507b3ff7bb6f01a0c2430d802b27
Raam R <raam.ratt@gmail.com>
2016-03-03 22:56:38 -0800

    Added intents to home buttons

--- Gysgo/app/src/main/java/com/gysgo/raam/gysgo/HomeActivity.java
+++ Gysgo/app/src/main/java/com/gysgo/raam/gysgo/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.gysgo.raam.gysgo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void onProfileClick(View view){
+        Intent intent = new Intent(this,ProfileActivity.class);
+        startActivity(intent);
+    }
+    public void onContactClick(View view){
+        Intent intent = new Intent(this,ContactActivity.class);
+        startActivity(intent);
+    }
+    public void onViewListClick(View view){
+        Intent intent = new Intent(this,ViewListActivity.class);
+        startActivity(intent);
+    }
+    public void onSignOutClick(View view){
+        //Intent intent = new Intent(this,ProfileActivity.class);
+        //startActivity(intent);
     }
 }
